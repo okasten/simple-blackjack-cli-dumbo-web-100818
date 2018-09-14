bde332f0243e3a3e96a79733edbe56660735d8e0
Olivia Kasten <odkasten@gmail.com>
2018-09-14 00:52:30 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,8 +35,14 @@ def initial_round
   return sum
 end
 
-def hit?
+def hit?(total)
   # code hit? here
+  prompt_user
+  answer = get_user_input
+  if answer == "h"
+    total += deal_card
+  elsif answer != "h" && answer != "s"
+  end 
 end
 
 def invalid_command
